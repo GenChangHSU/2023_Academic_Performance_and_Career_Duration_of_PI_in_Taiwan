4ca8410c94a64a028049cb1d5e05b702f6b89a9b
Gen-Chang Hsu <genchanghsu@gmail.com>
2023-01-03 12:19:38 +0800

    u

--- R Code/03_Figures.R
+++ R Code/03_Figures.R
@@ -4,7 +4,7 @@
 ##
 ## Author: Gen-Chang Hsu
 ##
-## Date: 2022-12-31
+## Date: 2023-01-03
 ##
 ## Description:
 ## 1. 
@@ -17,7 +17,6 @@ set.seed(123)
 # Libraries --------------------------------------------------------------------
 library(tidyverse)
 library(patchwork)
-library(ggsci)
 
 
 # Import files -----------------------------------------------------------------
@@ -109,7 +108,9 @@ P_performance_recruitment <- ggplot(data = filter(PI_df, beforeafter == "before"
         legend.position = c(0.32, 0.9),
         legend.direction = "horizontal",
         legend.key.width = unit(0.3, "inch"),
-        legend.text = element_text(margin = margin(r = 10, unit = "pt")))
+        legend.text = element_text(margin = margin(r = 10, unit = "pt"))) + 
+  annotate(geom = "text", x = 1995, y = 20, label = "italic(P) < 0.001", 
+           parse = T, size = 5)
   
 P_performance_recruitment
 
@@ -122,7 +123,9 @@ P_performance_promotion <- ggplot(data = filter(PI_df, beforeafter == "before" &
   scale_x_continuous(limits = c(1990, 2020), breaks = c(1990, 2000, 2010, 2020)) +
   scale_y_continuous(limits = c(0, 25)) + 
   my_theme + 
-  theme(plot.title = element_text(vjust = 1, hjust = 0))
+  theme(plot.title = element_text(vjust = 1, hjust = 0)) + 
+  annotate(geom = "text", x = 1995, y = 20, label = "italic(P) < 0.001", 
+           parse = T, size = 5)
 
 P_performance_promotion
 
@@ -135,7 +138,9 @@ P_duration_recruitment <- ggplot(data = filter(PI_df, beforeafter == "before" & 
   scale_x_continuous(limits = c(1990, 2020), breaks = c(1990, 2000, 2010, 2020)) +
   scale_y_continuous(limits = c(0, 11.5)) + 
   my_theme + 
-  theme(plot.title = element_text(vjust = 1, hjust = 0))
+  theme(plot.title = element_text(vjust = 1, hjust = 0)) + 
+  annotate(geom = "text", x = 1995, y = 9.3, label = "italic(P) < 0.001", 
+           parse = T, size = 5)
 
 P_duration_recruitment
 
@@ -148,7 +153,9 @@ P_duration_promotion <- ggplot(data = filter(PI_df, beforeafter == "before" & st
   scale_x_continuous(limits = c(1990, 2020), breaks = c(1990, 2000, 2010, 2020)) +
   scale_y_continuous(limits = c(0, 25)) + 
   my_theme + 
-  theme(plot.title = element_text(vjust = 1, hjust = 0))
+  theme(plot.title = element_text(vjust = 1, hjust = 0)) + 
+  annotate(geom = "text", x = 1995, y = 20, label = "italic(P) == 0.02", 
+           parse = T, size = 5)
 
 P_duration_promotion
 
@@ -167,6 +174,76 @@ PI_diff_df <- PI_df %>%
   mutate(recruitment_diff = assistant_after - assistant_before,
          promotion_diff = full_after - full_before)
 
+P_diff_recruitment <- ggplot(data = PI_diff_df) + 
+  geom_point(aes(x = Assistant.since, y = recruitment_diff)) + 
+  geom_smooth(aes(x = Assistant.since, y = recruitment_diff), method = "lm",
+              se = T, color = "black") + 
+  labs(x = "Year of recruitment", y = "Difference in academic performance (h-index)",
+       title = "(a)") +
+  scale_x_continuous(limits = c(1990, 2020), breaks = c(1990, 2000, 2010, 2020)) +
+  scale_y_continuous(limits = c(-10, 22)) + 
+  my_theme + 
+  theme(plot.title = element_text(vjust = 1, hjust = 0)) + 
+  annotate(geom = "text", x = 1995, y = 20, label = "italic(P) < 0.001", 
+           parse = T, size = 5)
 
+P_diff_recruitment
+
+P_diff_recruitment_botplot <- ggplot(data = PI_diff_df) + 
+  geom_boxplot(aes(x = PhD.taiwan, y = recruitment_diff, fill = PhD.taiwan),
+               color = "black", show.legend = F, outlier.color = NA, size = 1.3,
+               width = 0.6) + 
+  geom_point(aes(x = PhD.taiwan, y = recruitment_diff), color = "grey50", alpha = 0.5,
+             position = position_jitter(width = 0.1)) + 
+  labs(x = "Year of recruitment", y = "Difference in academic performance (h-index)",
+       title = "(b)") +
+  scale_y_continuous(limits = c(-10, 22)) +
+  scale_x_discrete(labels = c("Foreign", "Taiwan")) + 
+  scale_fill_manual(values = c("#d95f02", "#1b9e77")) +
+  my_theme + 
+  theme(plot.title = element_text(vjust = 1, hjust = 0)) + 
+  annotate(geom = "text", x = 1.5, y = 20, label = "italic(P) == 0.52", 
+           parse = T, size = 5)
+
+P_diff_recruitment_botplot 
+
+P_diff_promotion <- ggplot(data = PI_diff_df) + 
+  geom_point(aes(x = full.professor, y = promotion_diff)) + 
+  geom_smooth(aes(x = full.professor, y = promotion_diff), method = "lm",
+              se = F, color = "black", linetype = "dashed") + 
+  labs(x = "Year of promotion", y = "Difference in academic performance (h-index)",
+       title = "(c)") +
+  scale_x_continuous(limits = c(1990, 2020), breaks = c(1990, 2000, 2010, 2020)) +
+  scale_y_continuous(limits = c(-10, 10)) + 
+  my_theme + 
+  theme(plot.title = element_text(vjust = 1, hjust = 0)) + 
+  annotate(geom = "text", x = 1995, y = 8, label = "italic(P) == 0.09", 
+           parse = T, size = 5)
+
+P_diff_promotion
+
+P_diff_promotion_botplot <- ggplot(data = PI_diff_df) + 
+  geom_boxplot(aes(x = PhD.taiwan, y = promotion_diff, fill = PhD.taiwan),
+               color = "black", show.legend = F, outlier.color = NA, size = 1.3,
+               width = 0.6) + 
+  geom_point(aes(x = PhD.taiwan, y = promotion_diff), color = "grey50", alpha = 0.5,
+             position = position_jitter(width = 0.1)) + 
+  labs(x = "Year of promotion", y = "Difference in academic performance (h-index)",
+       title = "(d)") +
+  scale_y_continuous(limits = c(-10, 10)) +
+  scale_x_discrete(labels = c("Foreign", "Taiwan")) + 
+  scale_fill_manual(values = c("#d95f02", "#1b9e77")) +
+  my_theme + 
+  theme(plot.title = element_text(vjust = 1, hjust = 0)) + 
+  annotate(geom = "text", x = 1.5, y = 8, label = "italic(P) == 0.06", 
+           parse = T, size = 5)
+
+P_diff_promotion_botplot 
+
+P_diff_recruitment + P_diff_recruitment_botplot + 
+  P_diff_promotion + P_diff_promotion_botplot + 
+  plot_layout(nrow = 2)
+
+ggsave("./Outputs/Figures/diff_scatterplot_boxplot.tiff", width = 10, height = 9.5, dpi = 600, device = "tiff")
 
 

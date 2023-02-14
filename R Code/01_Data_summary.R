df8d0771c8e38f9b3d7f6bf720edc0048f963f10
Gen-Chang Hsu <genchanghsu@gmail.com>
2023-02-14 18:34:47 +0800

    u

--- R Code/01_Data_summary.R
+++ R Code/01_Data_summary.R
@@ -16,11 +16,12 @@ set.seed(123)
 # Libraries --------------------------------------------------------------------
 library(tidyverse)
 library(magrittr)
+library(readxl)
 
 
 # Import files -----------------------------------------------------------------
 PI_df <- read.csv("./Data_raw/publishperishdata.csv", header = T)
-
+other_PI_df <- read_xlsx("./Data_raw/other_PI_data.xlsx", sheet = 1)
 
 ############################### Code starts here ###############################
 
@@ -72,4 +73,42 @@ PI_df %>%
   group_by(University) %>% 
   summarize(n = n()) %>% 
   arrange(desc(n))
+
+
+# 2. Comparisons between the studied PIs and the entire PI population ----------
+### (1) Full data set of all EEB PIs in Taiwan
+all_PI_df <- PI_df %>% 
+  filter(stage == "assistant" & beforeafter == "before") %>%
+  select(University, Position, sex) %>% 
+  mutate(university_type = "public",
+         data_status = "included") %>% 
+  bind_rows(other_PI_df %>% 
+              select(University, Position, sex, university_type) %>% 
+              mutate(data_status = "not-included"))
+
+### (2) Number of public universities at which all the EEB PIs are
+all_PI_df %>% 
+  filter(university_type == "public") %$%
+  unique(University)
+
+### (3) Number of all EEB PIs from public universities
+all_PI_df %>% 
+  filter(university_type == "public") %>% 
+  nrow()
+
+### (4) Gender composition of all EEB PIs from public universities
+all_PI_df %>% 
+  filter(university_type == "public") %>%
+  count(sex)
+
+### (5) Academic rank composition of PIs from public universities
+all_PI_df %>% 
+  filter(university_type == "public") %>% 
+  count(Position)
+
+
+
+
+
+
 
